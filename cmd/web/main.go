9e0f708a6feb3002dc11f18860171b85bbba1c43
Manish Girdhar <manish.girdhar@hotmail.com>
2022-09-26 13:00:14 +1000

    feat: Initial Commit

---
+++ cmd/web/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/girdhar1982/appointment-system/config"
+	"github.com/girdhar1982/appointment-system/pkg/handlers"
+	"github.com/girdhar1982/appointment-system/pkg/render"
+)
+
+const portNumber = ":8080"
+var app config.AppConfig; //global variable available to every part
+var session *scs.SessionManager;
+// each file must have main
+func main() {
+  app.InProduction =false
+  session = scs.New()
+  session.Lifetime= 24 * time.Hour;
+  session.Cookie.Persist = true;
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+  app.Session = session;
+ //get the TemplateCache for the app config
+  tc, err := render.CreateTemplateCache();
+	app.TemplateCache=tc;
+	app.UseCache=false;
+	repo:=handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+	if err != nil {log.Fatal(err)}
+	fmt.Println("Starting Application on Port: ",portNumber)
+	srv := &http.Server{Addr: portNumber,Handler: routes(&app)}
+  err=srv.ListenAndServe()
+	if err != nil {log.Fatal(err)}
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"net/http"
+	"github.com/girdhar1982/appointment-system/config"
+	"github.com/girdhar1982/appointment-system/pkg/models"
+	"github.com/girdhar1982/appointment-system/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Respository
+
+// Repository is the repository type
+type Respository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Respository {
+	return &Respository{App: a}
+}
+
+// New Handlers sets the repository for the handlers
+func NewHandlers(r *Respository) {
+	Repo = r
+}
+
+func (m *Respository) Home(w http.ResponseWriter, r *http.Request) {
+	removeIP:=r.RemoteAddr
+	m.App.Session.Put(r.Context(),"remote_ip",removeIP)
+	render.RenderTemplates(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Respository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, there!!"
+
+	removeIP:=m.App.Session.GetString(r.Context(),"remote_ip")
+	stringMap["remote_ip"] = removeIP
+	render.RenderTemplates(w, "about.page.tmpl",&models.TemplateData{StringMap: stringMap,})
+}
