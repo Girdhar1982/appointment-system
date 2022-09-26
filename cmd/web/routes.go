26883146b45c6e28f9fe1d12c50c6aee1d1addf6
Manish Girdhar <manish.girdhar@hotmail.com>
2022-09-26 21:03:24 +1000

    html files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,7 +15,7 @@ mux.Use(NoSurf)
 mux.Use(SessionLoad)
 mux.Get("/",handlers.Repo.Home)
 mux.Get("/about",handlers.Repo.About)
-fileServer:= http.FileServer(http.Dir("./static/"))
-mux.Handle("./static/*",http.StripPrefix("/static",fileServer));
+fileServer := http.FileServer(http.Dir("./static/"))
+mux.Handle("/static/*",http.StripPrefix("/static",fileServer));
 return mux;
 }
